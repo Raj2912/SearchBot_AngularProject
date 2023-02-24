b106c478400e311a4bbeb80ddf8054139e1050cf
Raj Kumar <raj.kumar@glovoapp.com>
2023-02-24 15:37:57 +0100

    Use ngmodel to read input

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -10,15 +10,7 @@ export class WelcomePageComponent {
   textboxValue: string = '';
   constructor( public coreService: CoreserviceService) {}
 
-  ngOnInit(){
-    console.log(this.textboxValue);
-    // this.coreService.getData().subscribe(data => {
-    //   console.log(data);
-    // });
-  }
-
   onKey(event: any) { // without type info
-    this.textboxValue = event.target.value;
     console.log(this.textboxValue)
   }
 }
