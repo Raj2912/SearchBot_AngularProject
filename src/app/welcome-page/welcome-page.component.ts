3702e99fca471eed34aaada6edef92da28818b84
Raj2912 <rajsingh29012@gmail.com>
2023-03-03 11:21:09 +0530

    PUSH for saveUrl and SearchUrl

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CoreserviceService } from '../Services/coreservice.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 
 @Component({
@@ -12,9 +13,20 @@ export class WelcomePageComponent {
   textboxValue: string = '';
   constructor(  private http: HttpClient, public coreService: CoreserviceService) {}
 
-  onKey(event: any) { 
-    let responseObserver = this.http.get('http://localhost:8000/search/?search=raj');
+  SearchUrl(searchText: any) { 
+    let responseObserver = this.http.get('http://localhost:8000/search/?search='+searchText);
     responseObserver.subscribe(() => {});
     console.log(this.textboxValue)
   }
+
+
+  SaveUrl(saveText: any) { 
+    let saveresponse = this.http.get('http://localhost:8000/save/?save='+saveText);
+    saveresponse.subscribe(() => {});
+    console.log(this.textboxValue)
+  }
+
+
+
+  
 }
